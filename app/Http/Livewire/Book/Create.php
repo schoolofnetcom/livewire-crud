249cab454b713c335ab9c2a56181eb1c6e42b4c0
thivalls <olavalls@gmail.com>
2021-01-12 17:05:45 -0300

    Aula 5 - Criando primeiro registro

--- app/Http/Livewire/Book/Create.php
+++ app/Http/Livewire/Book/Create.php
@@ -3,9 +3,26 @@
 namespace App\Http\Livewire\Book;
 
 use Livewire\Component;
-
+use App\Models\Book;
 class Create extends Component
 {
+    public Book $book;
+
+    protected $rules = [
+        'book.name' => 'required|string',
+        'book.pages' => 'required|integer',
+        'book.author' => 'required|string',
+    ];
+
+    public function mount() {
+        $this->book = new Book();
+    }
+
+    public function save() {
+        Book::create($this->book->toArray());
+        return redirect()->route('books.index');
+    }
+
     public function render()
     {
         return view('livewire.book.create');
--- resources/views/livewire/book/create.blade.php
+++ resources/views/livewire/book/create.blade.php
@@ -1,3 +1,23 @@
 <div>
-    <p>CREATE LIVEWIRE</p>
+    <form wire:submit.prevent="save" class="w-full">
+	    <div class="w-full px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="name">
+                Nome
+            </label>
+            <input wire:model="book.name" class="appearance-none block w-full bg-gray-100 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" id="name" />
+    	</div>
+        <div class="w-full px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="pages">
+                Quantidade de Páginas
+            </label>
+            <input wire:model="book.pages" class="appearance-none block w-full bg-gray-100 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="number" id="pages" />
+    	</div>
+        <div class="w-full px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="author">
+                Autor
+            </label>
+            <input wire:model="book.author" class="appearance-none block w-full bg-gray-100 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" id="author" />
+    	</div>
+        <button type="submit">Cadastrar</button>
+    </form>
 </div>
